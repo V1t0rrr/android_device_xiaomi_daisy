354b3db4bdac7c202a053ee521f217e8b0829152
abhinavgupta371 <abhinavgupta371@gmail.com>
2021-07-17 00:20:54 +0000

    daisy: libhidl: build vendor variant of android.hidl.base@1.0

--- device.mk
+++ device.mk
@@ -273,6 +273,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
+    android.hidl.base@1.0_vendor \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system \
     libhidltransport \
