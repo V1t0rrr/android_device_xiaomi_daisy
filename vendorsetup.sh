ea5b44f0d821feb8340950a8e3a8ecaa041690ad
vitor00almei <pondebatata@outlook.com>
2021-07-20 03:10:28 +0000

    daisy: include vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/AOSPA/android_hardware_qcom_display.git -b ruby-89xx hardware/qcom/display
+
+git clone https://github.com/AOSPA/android_hardware_qcom_media.git -b ruby-89xx hardware/qcom/media
+
+git clone https://github.com/AOSPA/android_hardware_qcom_gps.git -b ruby-89xx hardware/qcom/gps
+
+git clone https://github.com/AOSPA/android_hardware_qcom_audio.git -b ruby-89xx vendor/qcom/opensource/audio-hal/primary-hal
+
+git clone https://github.com/vitor00almei/kernel_xiaomi_msm8953.git kernel/xiaomi/msm8953
